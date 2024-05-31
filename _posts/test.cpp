dc3ab3e7198b06fdafe50942da9d54d54213dca8
notj-code <junwonkim04@outlook.com>
2024-05-31 13:08:19 +0800

    editing blog page

--- _posts/test.cpp
+++ _posts/test.cpp
@@ -2,15 +2,22 @@
 using namespace std;
 
 int main() {
-	int N;
-	cin >> N;
-	vector<int> arr(N);
-	for (int i = 0; i < N; i++) cin >> arr[i];
-	sort(arr.begin(), arr.end());
-	int ans = 1;
-	for (int i = 1; i < N; i++) {
-		ans += (arr[i] != arr[i - 1]);
+	int n;
+	cin >> n;
+	vector<int> v(n);
+	for (int i=0; i<n; i++) {
+		v[i] = i+1;
 	}
-	cout << ans << "\n";
-	return 0;
+	if (n<4) {
+		cout << "NO SOLUTION";
+	} else if (n%2==0) {
+		for (int i=0;i<n/2;i++) {
+			cout << v[i] << " " << v[i+n/2] << " ";
+		}
+	} else if (n%2!=0) {
+			for (int i=0; i<(n-1)/2; i++) {
+				cout << v[i] << " " <<v[n-i-1]<< " ";
+			}
+			cout << v[(n-1)/2] << " ";
+		}
 }
