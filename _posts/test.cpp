e500047bf835658d786012971d0b6b850649c3de
notj-code <junwonkim04@outlook.com>
2024-05-31 13:24:02 +0800

    editing the page

--- _posts/test.cpp
+++ _posts/test.cpp
@@ -1,23 +1,27 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+
 using namespace std;
 
-int main() {
-	int n;
-	cin >> n;
-	vector<int> v(n);
-	for (int i=0; i<n; i++) {
-		v[i] = i+1;
-	}
-	if (n<4) {
-		cout << "NO SOLUTION";
-	} else if (n%2==0) {
-		for (int i=0;i<n/2;i++) {
-			cout << v[i] << " " << v[i+n/2] << " ";
-		}
-	} else if (n%2!=0) {
-			for (int i=0; i<(n-1)/2; i++) {
-				cout << v[i] << " " <<v[n-i-1]<< " ";
-			}
-			cout << v[(n-1)/2] << " ";
-		}
+int main()
+{
+  long long n;cin>>n;
+  if (n == 1)
+  {
+    cout << n << "\n";
+    exit(0);
+  }
+  if (n <= 3)
+      cout << "NO SOLUTION" << endl;
+  else
+  {
+    int start = n & 1 ? n - 1:n;
+    for (int i = start - 1; i > 0; i = i - 2)
+      cout << i << " ";
+    for (int i = start; i >= 2; i = i - 2)
+      cout << i << " ";
+    if (n & 1)
+      cout << n;
+    cout << "\n";
+  }
+  return 0;
 }
