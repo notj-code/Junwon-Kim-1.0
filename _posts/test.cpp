35881bbc025a13af99d0528765e4558bcc22f405
notj-code <junwonkim04@outlook.com>
2024-07-22 18:07:10 +0900

    new

--- _posts/test.cpp
+++ _posts/test.cpp
@@ -1,10 +1,32 @@
 #include<bits/stdc++.h>
-
 using namespace std;
 
 int main() {
-  long long n;
+  int arr[100][100], n, cnt=0;
+  for (int i=0; i<100; i++) {
+    for (int j=0; j<100; j++) {
+      arr[i][j] = 1;
+    }
+  }
   cin >> n;
-  if (n==0) {cout << 0;} 
-  else if (n>0) {cout << 4*n;}
+  int l[n], d[n];
+  for (int i=0; i<n; i++) {
+    cin >>l[i] >> d[i];
+  }
+
+  for (int i=0; i<n; i++) {
+    for (int j=0; j<l[i]; j++) {
+      for (int k=0; k<d[i]; k++) {
+        arr[j][k] = 0;
+      }
+    } 
+  }
+
+  for (int i=0; i<100; i++) {
+    for (int j=0; j<100; j++) {
+      cnt += arr[i][j];
+    }
+  }
+
+  cout << cnt;
 }
